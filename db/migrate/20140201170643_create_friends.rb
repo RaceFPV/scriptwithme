9bb4f7a09b89fecf239cbe1d6409426210a32123
slacker87 <racethesunlive@yahoo.com>
2014-04-26 07:03:04 +0000

    fix for postgres

--- db/migrate/20140201170643_create_friends.rb
+++ db/migrate/20140201170643_create_friends.rb
@@ -1,7 +1,7 @@
 class CreateFriends < ActiveRecord::Migration
   def change
     create_table :friends do |t|
-      t.references :user, index: true
+      t.string :user_id
       t.string :friend
 
       t.timestamps
